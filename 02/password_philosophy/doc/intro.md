627b3ea046cc42d69a78b7a62b8013c903cf865a
kungfu-lambda <kungfulambda@gmail.com>
2021-04-08 23:52:33 +0300

    day 2 init

---
+++ 02/password_philosophy/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to password_philosophy
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ 02/password_philosophy/src/password_philosophy/core.clj
@@ -0,0 +1,7 @@
+(ns password-philosophy.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
