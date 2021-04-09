eece2775bf2cc60c11db9e437f02b654ed965e3e
kungfu-lambda <kungfulambda@gmail.com>
2021-04-09 23:43:49 +0300

    day 2 part 2 completed

--- 02/password_philosophy/src/password_philosophy/core.clj
+++ 02/password_philosophy/src/password_philosophy/core.clj
@@ -3,15 +3,40 @@
   (:require [clojure.string :as cstr]
             [clojure.java.io :as io]))
 
-(defn analyze-line [line-str]
+(defn parse-line [line-str]
   (let [line (cstr/replace-first line-str #":" "")
         [min-max letter passwd] (cstr/split line #" ")
-        [min max] (map #(Integer/parseInt %) (cstr/split min-max #"-"))
-        cnt (get (frequencies passwd) (first letter) 0)
-        valid? (and (<= min cnt) (>= max cnt))]
+        [min max] (map #(Integer/parseInt %)
+                       (cstr/split min-max #"-"))]
     {:letter letter :passwd passwd
-     :min min :max max
-     :cnt cnt :valid? valid?}))
+     :min min :max max}))
+
+(defn valid?-1
+  "Validation for the part-1"
+  [{:keys [passwd letter min max] :as parsed}]
+  (let [cnt (get (frequencies passwd) (first letter) 0)
+        valid? (and (<= min cnt) (>= max cnt))]
+    valid?))
+
+(defn valid?-2
+  "Validation for the part-2"
+  [{:keys [passwd letter min max] :as parsed}]
+  (let [min (dec min)
+        max (dec max)
+        letters (vec (map str passwd))
+        minl (letters min)
+        maxl (letters max)]
+    (and (or (= minl letter) (= maxl letter))
+         (not= minl maxl))))
+
+(defn find-answers [parsed-lines]
+  (reduce (fn [{:keys [cnt1 cnt2]} parsed]
+            (let [v?1 (valid?-1 parsed)
+                  v?2 (valid?-2 parsed)]
+              {:cnt1 (if v?1 (inc cnt1) cnt1)
+               :cnt2 (if v?2 (inc cnt2) cnt2)}))
+          {:cnt1 0 :cnt2 0}
+          parsed-lines))
 
 (defn read-input
   "Reads input resource and returns lines as vector"
@@ -21,13 +46,9 @@
        slurp
        cstr/split-lines))
 
-(defn count-valids [lines]
-  (->> lines
-       (map analyze-line)
-       (filter #(:valid? %))
-       count))
-
 (defn -main
   "Automatically returns part-1 result"
   [& args]
-  (count-valids (read-input)))
+  (->> (read-input)
+       (map parse-line)
+       find-answers))
