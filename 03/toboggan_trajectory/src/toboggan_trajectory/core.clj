3597af599e146d17353ae70d74c330dbc2b0df16
kungfu-lambda <kungfulambda@gmail.com>
2021-04-11 18:25:27 +0300

    day 3 part 2 is completed

--- 03/toboggan_trajectory/src/toboggan_trajectory/core.clj
+++ 03/toboggan_trajectory/src/toboggan_trajectory/core.clj
@@ -9,21 +9,35 @@
        slurp
        cstr/split-lines))
 
-(defn count-trees-1 
-  ([lines] (count-trees-1 (rest lines) 0 0))
-  ([lines x cnt]
+(defn count-trees
+  "down is step size going down,
+   right and x-step is step size going right.
+   x is the current location on the x axis, cnt is
+   the ecountered tree count."
+  ([lines down right] 
+   (count-trees (->> lines (take-nth down) (drop 1)) right 0 0))
+  ([lines x-step x cnt]
    (let [[current & remaining] lines
          points (cstr/split current #"")
-         ndx (mod (+ 3 x) (count points))
+         ndx (mod (+ x-step x) (count points))
          tree? (= (points ndx) "#")
          cnt (if tree? (inc cnt) cnt)]
      (if (= remaining nil)
        cnt
-       (recur remaining ndx cnt)))))
+       (recur remaining x-step ndx cnt)))))
+
+(defn counting-part-2 [lines part1]
+  (let [a (count-trees lines 1 1)
+        b part1
+        c (count-trees lines 1 5)
+        d (count-trees lines 1 7)
+        e (count-trees lines 2 1)]
+    (* a b c d e)))
 
 (defn find-answers [lines]
-  (let [cnt1 (count-trees-1 lines)]
-    {:cnt1 cnt1}))
+  (let [part-1 (count-trees lines 1 3)
+        part-2 (counting-part-2 lines part-1)]
+    {:part-1 part-1 :part-2 part-2}))
 
 (defn -main
   "main."
